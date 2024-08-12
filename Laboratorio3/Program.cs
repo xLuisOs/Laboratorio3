d17282c6fc8dd44419edae172a52bb81eb4ff680
Luis Granados <luisoswaldogra@gmail.com>
2024-08-12 08:56:33 -0600

    Creacion del Menu

--- Laboratorio3/Program.cs
+++ Laboratorio3/Program.cs
@@ -2,13 +2,44 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("MENU");
-        Console.WriteLine("1. Registarar Cliente ");
-        Console.WriteLine("2. Regustrar Vehiculo ");
-        Console.WriteLine("3. Registrar Pedido ");
-        Console.WriteLine("4. Mostrar Detalles ");
-        Console.WriteLine("5. Buscar ");
-        Console.WriteLine("6. Salir");
+        int opcion = 0;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("MENU");
+            Console.WriteLine("1. Registarar Cliente ");
+            Console.WriteLine("2. Regustrar Vehiculo ");
+            Console.WriteLine("3. Registrar Pedido ");
+            Console.WriteLine("4. Mostrar Detalles ");
+            Console.WriteLine("5. Buscar ");
+            Console.WriteLine("6. Salir");
+            Console.WriteLine("Ingrese una opcion");
+            try
+            {
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch(opcion)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        break;
+                }
 
+            }catch (Exception ex)
+            {
+
+            }
+            
+        } while (opcion != 6);
     }
 }
