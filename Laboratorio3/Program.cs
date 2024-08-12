f6728bbd3eb45783f47ed0cf6239e69b8d02838d
Luis Granados <luisoswaldogra@gmail.com>
2024-08-12 08:45:57 -0600

    Add project files.

---
+++ Laboratorio3/Program.cs
@@ -0,0 +1,14 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("MENU");
+        Console.WriteLine("1. Registarar Cliente ");
+        Console.WriteLine("2. Regustrar Vehiculo ");
+        Console.WriteLine("3. Registrar Pedido ");
+        Console.WriteLine("4. Mostrar Detalles ");
+        Console.WriteLine("5. Buscar ");
+        Console.WriteLine("6. Salir");
+
+    }
+}
